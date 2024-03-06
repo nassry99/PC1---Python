c13116456b085352156797817f443632a9145b97
nassry99 <162034473+nassry99@users.noreply.github.com>
2024-03-06 16:36:43 +0000

    SOLUCION 7

--- EJERCICIO7.py
+++ EJERCICIO7.py
@@ -1,7 +1,8 @@
-
-
-
-
+#Realiza un programa que lea dos números por teclado y permite elegir entre 3 opciones en un menú:
+#- Mostrar una suma de los dos números
+#- Mostrar una resta de los dos números (el primero menos el segundo)
+#- Mostrar una multiplicación de los dos números
+#- En caso de introducir una opción inválida, el programa informará de que no es correcta.
 
 # Función para mostrar el menú
 def mostrar_menu():
@@ -11,20 +12,20 @@ def mostrar_menu():
     print("3. Mostrar multiplicación de los dos números")
 
 # Función para realizar la suma
-def suma(num1, num2):
-    return num1 + num2
+def suma(a, b):
+    return a+b
 
 # Función para realizar la resta
-def resta(num1, num2):
-    return num1 - num2
+def resta(a, b):
+    return a-b
 
 # Función para realizar la multiplicación
-def multiplicacion(num1, num2):
-    return num1 * num2
+def multiplicacion(a, b):
+    return a*b
 
 # Leer dos números ingresados por el usuario
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
 
 # Mostrar el menú
 mostrar_menu()
@@ -34,10 +35,10 @@ opcion = input("Ingrese el número de opción deseada: ")
 
 # Realizar la operación correspondiente según la opción seleccionada
 if opcion == '1':
-    print("La suma de los dos números es:", suma(num1, num2))
+    print("La suma de los dos números es:", suma(a, b))
 elif opcion == '2':
-    print("La resta de los dos números es:", resta(num1, num2))
+    print("La resta de los dos números es:", resta(a, b))
 elif opcion == '3':
-    print("La multiplicación de los dos números es:", multiplicacion(num1, num2))
+    print("La multiplicación de los dos números es:", multiplicacion(a, b))
 else:
     print("Opción inválida. Por favor, elija una opción válida.")
