b8ade0ff298ce7bb6e8f9b3fe151912b078c1f24
nassry99 <162034473+nassry99@users.noreply.github.com>
2024-03-06 19:08:08 +0000

    SOLUCION 9

---
+++ EJERCICIO9.py
@@ -0,0 +1,11 @@
+#Escriba un programa que, dada una lista, devuelve una nueva lista cuyo contenido sea igual a la
+#original pero invertida. Así, dada la lista ['Di', 'buen', 'día', 'a', 'papa'], deberá devolver ['papa', 'a',
+#'día', 'buen', 'Di'].
+
+#definir lista 
+listaoriginal=['Di', 'buen', 'día', 'a', 'papa']
+#lista invertidad
+lista_invert=list(reversed(listaoriginal))
+
+#mostrar los resultados 
+print(lista_invert)
